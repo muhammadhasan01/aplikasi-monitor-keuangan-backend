d37dc7d983f6ecdbcc008a322f76533eac2f9434
Muhamamad Hasan <muhammadhasan50@gmail.com>
2021-03-22 13:55:28 +0700

    feat: add auth controller

---
+++ controller/auth-controller.js
@@ -0,0 +1,30 @@
+import {getToken} from "../auth/jwt-token.js";
+import {AccountModel} from "../models/account-model.js";
+
+export const login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        if (!username || !password) {
+            return res.status(400).json({
+               message: "Input fields cannot be empty"
+            });
+        }
+        const user = await AccountModel.findOne({ username: username });
+        console.log(user);
+        if (!user) {
+            return res.status(400).json({
+                message: "The given username was not found"
+            });
+        }
+        // TODO: Check password the same with bcrypt
+        if (password !== user.password) {
+            return res.status(400).json({
+               message: "Invalid credentials"
+            });
+        }
+        const token = getToken(user);
+        return res.status(200).send(token);
+    } catch (err) {
+        return res.status(500).send(err);
+    }
+}
