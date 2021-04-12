910048c8cdd28b208a5298f3cef775c542d585ed
fabianusharry <fabianusharry@yahoo.com>
2021-04-12 19:17:44 +0700

    Update RKA-controller.js

--- controller/RKA-controller.js
+++ controller/RKA-controller.js
@@ -49,8 +49,56 @@ export const getPenggunaanRKA = async (req, res) => {
     }
 };
 
-export const createRKA = async (req,res) => {
-    console.log("Bentar dulu")
+export const createRKA = async (req, res) => {
+    try {
+        const {year, ADO, kegiatan, subkegiatan, rincian_subkegiatan, jenis_belanja, satuan, volume, penggunaan} = req.body;
+        const unit = req.params.unit;
+        const sub_unit = req.params.subunit; 
+        const rincian_belanja = req.params.rincian;
+        if (!year || !unit || !sub_unit || !ADO || !kegiatan || !subkegiatan || !rincian_subkegiatan || !rincian_belanja || !jenis_belanja || !satuan || !volume || !penggunaan) {
+            print(req.body);
+            return res.status(400).send({
+                message: "required field cannot be empty"
+            });
+        }
+        const alokasi_ado = await pagu.getAlokasiPagu(unit, ADO, year);
+        const penggunaan_ado = await pagu.getPenggunaanPagu(unit, ADO, year);
+        const pengeluaran = penggunaan.januari + penggunaan.februari + penggunaan.maret + penggunaan.april + penggunaan.mei + penggunaan.juni + penggunaan.juli + penggunaan.agustus + penggunaan.september + penggunaan.oktober + penggunaan.november + penggunaan.desember;
+        if ((pengeluaran + penggunaan_ado) > alokasi_ado) {
+            return res.status(400).send({
+                message: "insufficient funds"
+            });
+        }
+        
+        const newRKA = await RKA.createRKA(unit, sub_unit, rincian_belanja, req.body);
+        const newPagu = await pagu.updatePagu(unit, ADO, year, (pengeluaran + penggunaan_ado));
+        return res.status(201).send(newRKA);
+    } catch (err) {
+        return res.status(500).send(err);
+    }
+};
+
+export const deleteRKA = async (req, res) => {
+    try {
+        const unit = req.params.unit;
+        const sub_unit = req.params.subunit; 
+        const rincian_belanja = req.params.rincian;
+        const rka = await RKA.getRKA(unit, sub_unit, rincian_belanja);
+        const year = rka.year;
+        const penggunaan = await RKA.getPengeluaranRKA(unit, sub_unit, rincian_belanja);
+        const pengeluaran = penggunaan.januari + penggunaan.februari + penggunaan.maret + penggunaan.april + penggunaan.mei + penggunaan.juni + penggunaan.juli + penggunaan.agustus + penggunaan.september + penggunaan.oktober + penggunaan.november + penggunaan.desember;
+        const penggunaan_ado = await pagu.getPenggunaanPagu(unit, ADO, year);
+
+        const deletedRKA = await RKA.deleteRKA(unit, sub_unit, rincian_belanja);
+        const newPagu = await pagu.updatePagu(unit, ADO, year, (penggunaan_ado - pengeluaran));
+        return res.status(200).send(deletedRKA);
+    } catch (err) {
+        if (err.name === "accountNotFound")
+            return res.status(404).send({
+                message: err.message
+            });
+        return res.status(500).send(err);
+    }
 }
 
 export default router
