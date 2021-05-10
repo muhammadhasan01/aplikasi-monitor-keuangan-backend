2a8001b1783f68d7c6e02ea1df7ee856d44a1cee
Fathan Mubina <13518064@std.stei.itb.ac.id>
2021-05-10 16:07:31 +0700

    forgot password feature

--- controller/auth-controller.js
+++ controller/auth-controller.js
@@ -1,5 +1,5 @@
 import {getToken} from "../auth/jwt-token.js";
-import {AccountModel, checkPassword} from "../models/account-model.js";
+import {AccountModel, checkPassword, getUsername} from "../models/account-model.js";
 
 export const login = async (req, res) => {
     try {
@@ -27,3 +27,67 @@ export const login = async (req, res) => {
         return res.status(500).send(err);
     }
 }
+
+
+export const resetPassword = async (req, res) => {
+    try {
+        const {username, newPassword, newPasswordConfirmation} = req.body;
+        if (newPassword != newPasswordConfirmation) {
+            return res.status(400).json({
+               message: "Password doesn't match"
+            });
+        }
+        else{
+            const user = getUsername(username);
+            if (!user) {
+                return res.status(400).json({
+                    message: "The given username was not found"
+                });
+            }
+            else{
+                user.password = newPassword
+                updateUser(user);
+            }
+            return res.status(200).json({
+                message: "Password has been successfully reset"
+            });
+        }
+    } catch (err) {
+        return res.status(500).send(err);
+    }
+}
+
+export const sendResetLink = async (req, res) => {
+    try {
+        const {username} = req.body;
+        const user = getUsername(username);
+        var nodemailer = require('nodemailer');
+
+        var transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+            user: 'steianggaran@gmail.com',
+            pass: 'Anggaran2021'
+          }
+        });
+        
+        var mailOptions = {
+          from: 'steianggaran@gmail.com',
+          to: user.email,
+          subject: 'Reset Password Monitoring Anggaran STEI ',
+          text: 'To reset your password, please click on this link: http://localhost:3000/reset/'+username
+        };
+        
+        transporter.sendMail(mailOptions, function(error, info){
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('Email sent: ' + info.response);
+          }
+        }); 
+    } catch (err) {
+        return res.status(500).send(err);
+    }
+}
+
+
--- routes/auth-router.js
+++ routes/auth-router.js
@@ -4,5 +4,8 @@ import * as Auths from '../controller/auth-controller.js';
 const router = express.Router();
 
 router.post('/login', Auths.login);
+router.post('/reset', Auths.reset);
+router.post('/sendResetLink', Auths.sendResetLink);
+router.post('/resetPassword', Auths.resetPassword );
 
-export default router;
+export default router; 
