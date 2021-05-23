57e6f161f1590cc7333c8cefc48a9c9c26065ce3
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2021-05-24 03:41:52 +0700

    chore: use prettier and remove console log

--- models/expense-history-model.js
+++ models/expense-history-model.js
@@ -1,22 +1,23 @@
 import mongoose from 'mongoose';
+
 let Schema = monggose.Schema;
 
 const expenseHistorySchema = Schema({
-    ID_RKA: {
-        type: Schema.Types.ObjectId,
-        required: true,
-    },
-    expense_date: {
-        type: Date,
-        unique: true,
-        default: Date.now(),
-    },
-    jumlah: {
-        type: Number,
-        required: true,
-    },
+  ID_RKA: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  expense_date: {
+    type: Date,
+    unique: true,
+    default: Date.now(),
+  },
+  jumlah: {
+    type: Number,
+    required: true,
+  },
 }, {
-    timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" }
+  timestamps: {createdAt: "createdAt", updatedAt: "updatedAt"}
 });
 
 const ExpenseHistoryModel = mongoose.model('expenseHistory', expenseHistorySchema);
