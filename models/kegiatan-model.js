518091782e702592483ddf9516811da1eb876812
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2021-05-24 03:41:59 +0700

    chore: use prettier and remove console log

--- models/kegiatan-model.js
+++ models/kegiatan-model.js
@@ -1,78 +1,54 @@
 import mongoose from 'mongoose';
 
 const kegiatanSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-    },
-    subkegiatan: {
-        type: String,
-        required: true,
-        trim: true,
-    }
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  subkegiatan: {
+    type: String,
+    required: true,
+    trim: true,
+  }
 });
 
 const kegiatanModel = mongoose.model('kegiatans', kegiatanSchema);
 
 export const getKegiatans = async () => {
-    try {
-        const kegiatans = await kegiatanModel.find();
-        return kegiatans;
-    } catch (err) {
-        throw err;
-    }
+  const kegiatans = await kegiatanModel.find();
+  return kegiatans;
 }
 
 export const getSubKegiatans = async () => {
-    try {
-        const subKegiatans = await kegiatanModel.distinct("subkegiatan");
-        return subKegiatans;
-    } catch (err) {
-        throw err;
-    }
+  const subKegiatans = await kegiatanModel.distinct("subkegiatan");
+  return subKegiatans;
 }
 
-export const createKegiatan = async ({ name, subkegiatan }) => {
-    const newKegiatan = new kegiatanModel({ name, subkegiatan });
-    try {
-        const kegiatanCreated = await newKegiatan.save();
-        return kegiatanCreated;
-    } catch (err) {
-        throw err;
-    }
+export const createKegiatan = async ({name, subkegiatan}) => {
+  const newKegiatan = new kegiatanModel({name, subkegiatan});
+  const kegiatanCreated = await newKegiatan.save();
+  return kegiatanCreated;
 }
 
 export const getKegiatan = async (id) => {
-    try {
-        const kegiatan = await kegiatanModel.findById(id);
-        if (!kegiatan) {
-            throw {name: "kegiatanNotFound", message: `kegiatan with ${id} was not found`};
-        }
-        return kegiatan;
-    } catch (err) {
-        throw err;
-    }
+  const kegiatan = await kegiatanModel.findById(id);
+  if (!kegiatan) {
+    throw {name: "kegiatanNotFound", message: `kegiatan with ${id} was not found`};
+  }
+  return kegiatan;
 }
 
 export const updateKegiatan = async (id, data) => {
-    try {
-        const updatedKegiatan = await kegiatanModel.findByIdAndUpdate(id, data, {new: true});
-        if (!updatedKegiatan) {
-            throw {name: "kegiatanNotFound", message: `kegiatan with ${id} was not found`};
-        }
-        return updatedKegiatan;
-    } catch (err) {
-        throw err;
-    }
+  const updatedKegiatan = await kegiatanModel.findByIdAndUpdate(id, data, {new: true});
+  if (!updatedKegiatan) {
+    throw {name: "kegiatanNotFound", message: `kegiatan with ${id} was not found`};
+  }
+  return updatedKegiatan;
 }
 
 export const deleteKegiatan = async (id) => {
-    try {
-        const deletedKegiatan = await kegiatanModel.findByIdAndRemove(id);
-        return deletedKegiatan;
-    } catch (err) {
-        throw err;
-    }
+  const deletedKegiatan = await kegiatanModel.findByIdAndRemove(id);
+  return deletedKegiatan;
 }
