d8e4a171b72e2816f29c26b7805be113eddefed2
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2021-05-24 03:42:19 +0700

    chore: use prettier and remove console log

--- models/units-model.js
+++ models/units-model.js
@@ -1,113 +1,76 @@
 import mongoose from 'mongoose';
 
 const unitsSchema = mongoose.Schema({
-    unit: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-    },
-    code: {
-        type: Number,
-        unique: true
-    },
-    subunit: {
-        type: String,
-        required: true,
-        trim: true,
-    }
+  unit: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  code: {
+    type: Number,
+    unique: true
+  },
+  subunit: {
+    type: String,
+    required: true,
+    trim: true,
+  }
 });
 
 const UnitsModel = mongoose.model('units', unitsSchema);
 
 export const getUnits = async () => {
-    try {
-        const units = await UnitsModel.find();
-        return units;
-    } catch (err) {
-        throw err;
-    }
+  const units = await UnitsModel.find();
+  return units;
 }
 
 export const getDistinctUnits = async () => {
-    try {
-        const units = await UnitsModel.distinct("unit");
-        return units;
-    } catch (err) {
-        throw err;
-    }
+  const units = await UnitsModel.distinct("unit");
+  return units;
 }
 
 export const getSubUnits = async () => {
-    try {
-        const subUnits = await UnitsModel.distinct("subunit");
-        return subUnits;
-    } catch (err) {
-        throw err;
-    }
+  const subUnits = await UnitsModel.distinct("subunit");
+  return subUnits;
 }
 
-export const getSubUnitsForUnits = async(unit) => {
-    try{
-        const subunits = await UnitsModel.distinct("subunit", { unit: unit })
-        return subunits;
-    }
-    catch(err){
-        throw err;
-    }
+export const getSubUnitsForUnits = async (unit) => {
+  const subunits = await UnitsModel.distinct("subunit", {unit: unit})
+  return subunits;
 }
 
-export const createUnit = async ({ unit, code, subunit }) => {
-    const newUnit = new UnitsModel({ unit, code, subunit });
-    try {
-        const unitCreated = await newUnit.save();
-        return unitCreated;
-    } catch (err) {
-        throw err;
-    }
+export const createUnit = async ({unit, code, subunit}) => {
+  const newUnit = new UnitsModel({unit, code, subunit});
+  const unitCreated = await newUnit.save();
+  return unitCreated;
 }
 
-export const isUnitExist = async(unit, code, subunit) => {
-    try{
-        const queryUnit = await UnitsModel.findOne({ unit: unit, code: code, subunit: subunit });
-        if (!queryUnit) {
-            return false;
-        }
-        return true;
-    } catch (err) {
-        throw err;
-    }
+export const isUnitExist = async (unit, code, subunit) => {
+  const queryUnit = await UnitsModel.findOne({unit: unit, code: code, subunit: subunit});
+  if (!queryUnit) {
+    return false;
+  }
+  return true;
 }
 
 export const getUnit = async (name) => {
-    try {
-        const unit = await UnitsModel.find({unit: name});
-        if (!unit) {
-            throw {name: "unitNotFound", message: `unit with ${id} was not found`};
-        }
-        return unit;
-    } catch (err) {
-        throw err;
-    }
+  const unit = await UnitsModel.find({unit: name});
+  if (!unit) {
+    throw {name: "unitNotFound", message: `unit with ${id} was not found`};
+  }
+  return unit;
 }
 
 export const updateUnit = async (id, data) => {
-    try {
-        const updatedUnit = await UnitsModel.findByIdAndUpdate(id, data, {new: true});
-        if (!updatedUnit) {
-            throw {name: "unitNotFound", message: `unit with ${id} was not found`};
-        }
-        return updatedUnit;
-    } catch (err) {
-        throw err;
-    }
+  const updatedUnit = await UnitsModel.findByIdAndUpdate(id, data, {new: true});
+  if (!updatedUnit) {
+    throw {name: "unitNotFound", message: `unit with ${id} was not found`};
+  }
+  return updatedUnit;
 }
 
 export const deleteUnit = async (id) => {
-    try {
-        const deletedUnit = await UnitsModel.findByIdAndRemove(id);
-        return deletedUnit;
-    } catch (err) {
-        throw err;
-    }
+  const deletedUnit = await UnitsModel.findByIdAndRemove(id);
+  return deletedUnit;
 }
